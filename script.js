7abaae98973c7402de5ad47a14463fcd6b062402
Irina <il064330@gmail.com>
2023-10-04 00:43:07 +0300

    create files

---
+++ script.js
@@ -0,0 +1,42 @@
+const button = document.querySelector('.button');
+const info = document.getElementById('info');
+const inputDate = document.querySelector('.inputDate');
+const mistake = document.getElementById('mistake');
+
+function countDaysTill () {
+  let birthday = document.getElementById('birthday').value;
+  let msBirthday = Date.parse(birthday);
+  let today = Date.now();
+  let daysTill = Math.floor((msBirthday - today) / (1000 * 60 * 60 * 24));
+  let result = document.getElementById('result');
+  result.textContent = daysTill;
+  info.style.visibility = "visible";
+
+  let days = document.getElementById('days');
+  let lastDigit = daysTill % 10;
+  let secondLastDigit = Math.floor((daysTill % 100) / 10);
+  if(secondLastDigit === 1) {
+    days.textContent = "дней";
+  } else {
+    if(lastDigit >= 2 && lastDigit <= 4) {
+      days.textContent = "дня";
+    } else {
+      if(lastDigit === 1) {
+        days.textContent = "день";
+      } else {
+      days.textContent = "дней";
+    }
+  }
+}
+}
+
+button.addEventListener('click', countDaysTill);
+
+inputDate.addEventListener("input",
+function() {
+    if(inputDate.value === "") {
+      mistake.style.display = "block";
+    } else {
+      mistake.style.display = "none";
+    }
+  });
